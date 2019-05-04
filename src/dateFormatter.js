2deb74b48ea02c2c06f12f1fef06259188c60d2d
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-05-04 01:28:47 +0100

    EXERCISE_19051 Solution

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,15 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  let result = '';
+  let dateToFormatTime = new Date(dateToFormatTimeMillis);
+  let formatDates =   dateToFormatTime.getDate() + "/" + (dateToFormatTime.getMonth() + 1)  +"/" + dateToFormatTime.getFullYear();
+  let systemDateTime = new Date(systemDateTimeMillis);
+  let systemDates = systemDateTime.getDate() + "/" + (systemDateTime.getMonth() + 1)  +"/" + systemDateTime.getFullYear();
+
+  if(formatDates == systemDates){
+    result = 'TODAY';
+  }else{
+    result = formatDates;
+  }
+
+  return result;
 };
